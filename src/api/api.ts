365de750cd3a5e221b36ac661047c0180394f516
Jakub Chatrný <jakub.chatrny@morosystems.cz>
2020-10-27 08:59:38 +0100

    solution added

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,3 +1,32 @@
 // TODO: Fetch data and return Stats object created from it
 // Make sure to correctly type this function, there should be no `any` type in your whole solution
-export const getPersonsStats = (): any => {};
+import {Stats, Course, CourseCode, Student, Teacher, Person, PersonType} from "./types";
+
+export const getPersonsStats = (): Promise<Stats> => (
+    fetch('http://localhost:8080/persons')
+        .then((response) => response.json())
+        .then((persons) => persons.reduce((acc: Stats, person: Person) => {
+            if (person.type === PersonType.STUDENT) {
+                (<Student>person)?.attends?.forEach((courseCode: CourseCode) => {
+                    if (acc[courseCode].students === undefined) {
+                        acc[courseCode].students = []
+                    }
+                    acc[courseCode]?.students?.push(person.name);
+                })
+            }
+            if (person.type === PersonType.TEACHER) {
+                (<Teacher>person)?.teaches?.forEach((courseCode: CourseCode) => {
+                    if(acc[courseCode].teachers === undefined) {
+                        acc[courseCode].teachers = []
+                    }
+                    acc[courseCode]?.teachers?.push(person.name);
+                })
+            }
+            return acc;
+        }, {
+            'A': {} as Course,
+            'B': {} as Course,
+            'C': {} as Course,
+            'D': {} as Course,
+        }))
+);
--- src/api/types.ts
+++ src/api/types.ts
@@ -1,14 +1,41 @@
 // String literal type consisting of all the course codes
 // Let's pretend this type is imported from our backend
-type CourseCode = "A" | "B" | "C" | "D";
+export type CourseCode = "A" | "B" | "C" | "D";
+export enum PersonType {
+    TEACHER = "teacher",
+    STUDENT = "student",
+}
 
 // TODO: Create Course interface object that contains optional arrays of teachers and students names
 // interface Course { ...
-
+export interface Course {
+    teachers?: Array<string>,
+    students?: Array<string>,
+}
 // TODO: Create Stats object type that keys of CourseCode will contain Course objects
-export type Stats = {}
+export type Stats = {
+    [key in CourseCode]: Course
+}
 
 // TODO: Create Student and Teacher interfaces (extending Person interface with common attributes)
 // interface Student extends ...
 // interface Teacher extends ...
+
+export interface Person {
+    id: string,
+    name: string,
+    type: PersonType,
+}
+
+export interface Student extends Person {
+    type: PersonType.STUDENT,
+    attends: Array<CourseCode>
+    semester: number
+}
+export interface Teacher extends Person {
+    type: PersonType.TEACHER,
+    teaches: Array<CourseCode>,
+}
+// {"id":"a","name":"John","type":"student","attends":["A","B","D"],"semester":1}
+// {"id":"4","name":"Mrs. Judy","type":"teacher","teaches":[]}
 
